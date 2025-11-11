685bf78a2ae83583362a449272fc4a52f1a3d621
TARUN836 <145276473+TARUN836@users.noreply.github.com>
2025-11-11 21:08:23 +0530

    Update app.py

--- student-management/app.py
+++ student-management/app.py
@@ -46,4 +46,7 @@ def view_students():
     return render_template('view_students.html', students=data)
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5001)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
